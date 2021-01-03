9ff42e60927a048c9f8f9972691f2dcd5c3bf670
Danny Moloney <G00359969@gmit.ie>
2021-01-03 01:20:30 +0000

    my first project commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Header } from './components/header';
+import { Footer } from './components/footer';
+import { Content } from './components/content';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Nav} from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <Router>
+      <div className="App">
+
+<Navbar bg="primary" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/features">Features</Nav.Link>
+      <Nav.Link href="/pricing">Pricing</Nav.Link>
+      <Nav.Link href="/entry">Entry</Nav.Link>
+    </Nav>
+  </Navbar>
+
+  <br />
+  <Switch>
+    <Route path="/" component={ Content } exact/>
+    <Route path="/features" component={ Header } exact/>
+    <Route path="/pricing" component={ Footer } exact/>
+
+  </Switch>
+      </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
